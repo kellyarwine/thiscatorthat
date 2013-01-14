dc9b3c246b9c08e9a7e238bcbe638c47eb2bd59d
Kelly Steensma <kellysteensma@gmail.com>
2013-01-14 16:07:08 -0600

    Made a bunch of changes

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -6,8 +6,8 @@ class CatsController < ApplicationController
     @cat1, @cat2 = Cat.two_random_cats
     p @cat1
     p @cat2
-    p @cat2
     @top_cat = Cat.top_cat
+    @cats = Cat.all
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @cats }
---
+++ db/migrate/20130114212552_add_name_columnto_cats.rb
@@ -0,0 +1,7 @@
+class AddNameColumntoCats < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
